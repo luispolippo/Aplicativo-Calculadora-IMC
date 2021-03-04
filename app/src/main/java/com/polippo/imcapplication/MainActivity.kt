0552d291493aa11ba31325e253502265d5eaabdb
Luis Guilherme Polippo <luispolippo@hotmail.com>
2021-03-04 02:51:01 -0300

    layout color changed added classification IMC some improvements

--- app/src/main/java/com/polippo/imcapplication/MainActivity.kt
+++ app/src/main/java/com/polippo/imcapplication/MainActivity.kt
@@ -1,16 +1,22 @@
 package com.polippo.imcapplication
 
+import android.graphics.Color
+import android.os.Build
 import android.os.Bundle
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.snackbar.Snackbar
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    @RequiresApi(Build.VERSION_CODES.M)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setListeners()
     }
 
+    @RequiresApi(Build.VERSION_CODES.M)
     private fun setListeners(){
 
         calcularButton?.setOnClickListener {
@@ -18,14 +24,54 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    @RequiresApi(Build.VERSION_CODES.M)
     private fun calcularIMC(peso:String, altura:String){
 
-        val peso = peso.toFloatOrNull()
-        val altura = altura.toFloatOrNull()
+        if((peso.isEmpty() || peso.isBlank()) && (altura.isEmpty() || altura.isBlank())){
+            Snackbar.make(findViewById(R.id.coordinatorLayout), getString(R.string.snackbar_text), Snackbar.LENGTH_SHORT).show()
+        } else {
 
-        if(peso != null && altura != null){
-            val imc = peso/ (altura * altura)
-            titleTxt.text = "Seu IMC é %.2f".format(imc)
+            val pesoFloat = peso.toFloatOrNull()
+            val alturaFloat = altura.toFloatOrNull()
+
+
+            if (pesoFloat != null && alturaFloat != null) {
+                val imc = pesoFloat / (alturaFloat * alturaFloat)
+                resultImcTxt.text = getString(R.string.imc_result).format(imc)
+
+                when {
+                    imc < 18.5 -> {
+                        resultTxt.text = getString(R.string.abaixo_peso)
+                        resultTxt.setTextColor(Color.RED)
+                        resultImcTxt.setTextColor(getColor(R.color.ic_launcher_background))
+                    }
+                    imc <= 24.9 -> {
+                        resultTxt.text = getString(R.string.peso_normal)
+                        resultTxt.setTextColor(Color.GREEN)
+                        resultImcTxt.setTextColor(getColor(R.color.ic_launcher_background))
+                    }
+                    imc <= 29.9 -> {
+                        resultTxt.text = getString(R.string.sobrepeso)
+                        resultTxt.setTextColor(Color.YELLOW)
+                        resultImcTxt.setTextColor(getColor(R.color.ic_launcher_background))
+                    }
+                    imc <= 34.9 -> {
+                        resultTxt.text = getString(R.string.obesidade_1)
+                        resultTxt.setTextColor(Color.RED)
+                        resultImcTxt.setTextColor(getColor(R.color.ic_launcher_background))
+                    }
+                    imc <= 39.9 -> {
+                        resultTxt.text = getString(R.string.obesidade_2)
+                        resultTxt.setTextColor(Color.RED)
+                        resultImcTxt.setTextColor(getColor(R.color.ic_launcher_background))
+                    }
+                    else -> {
+                        resultTxt.text = getString(R.string.obesidade_morbida)
+                        resultTxt.setTextColor(Color.RED)
+                        resultImcTxt.setTextColor(Color.RED)
+                    }
+                }
+            }
         }
 
     }
